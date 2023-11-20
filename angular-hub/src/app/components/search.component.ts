991ca91994fcda686affc9c24c033f91c613983d
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-11-20 21:29:42 +0100

    feat(content): add event search feature

--- angular-hub/src/app/components/search.component.ts
+++ angular-hub/src/app/components/search.component.ts
@@ -1,20 +1,80 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, forwardRef, signal} from "@angular/core";
 import {NgOptimizedImage} from "@angular/common";
+import {ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR} from "@angular/forms";
 
 @Component({
   selector: 'app-search',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   imports: [
-    NgOptimizedImage
+    NgOptimizedImage,
+    FormsModule
+  ],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => SearchComponent),
+      multi: true
+    }
   ],
   template: `
-    <div class="flex justify-between border-2 border-gray-500 rounded-lg p-1 mb-6">
-      <input type="text">
-      <img ngSrc="/icons/search.svg" height="20" width="20" alt="">
+    <div
+      class="flex justify-between border-2 border-gray-500 rounded-lg gap-4 p-1 mb-6"
+      [class.border-[#BF25B9]]="isFocused()"
+    >
+      <img ngSrc="/assets/icons/search.svg" height="20" width="20" alt="">
+      <input class="flex-1 outline-0 bg-transparent"
+             type="text"
+             aria-label="Search"
+             [(ngModel)]="searchTerm"
+             (ngModelChange)="onChange($event)"
+             (blur)="onBlur()"
+             (focus)="onFocus()"
+      >
+      <button
+        type="button"
+        aria-label="Reset search"
+        [style.visibility]="searchTerm.length ? 'visible' : 'hidden'"
+        [attr.aria-hidden]="!searchTerm.length"
+        (click)="reset()">
+        <img ngSrc="/assets/icons/cancel.svg" height="20" width="20" alt="">
+      </button>
     </div>
   `
 })
-export class SearchComponent {
-  // TODO : implement search
+export class SearchComponent implements ControlValueAccessor {
+  isFocused = signal(false);
+  searchTerm: string = '';
+  onChange: unknown = () => {
+  }
+  onTouch: unknown = () => {
+  }
+
+  writeValue(searchTerm: string): void {
+    this.searchTerm = searchTerm;
+  }
+
+  registerOnChange(fn: unknown): void {
+    this.onChange = fn;
+  }
+
+  registerOnTouched(fn: unknown): void {
+    this.onTouch = fn;
+  }
+
+  onFocus() {
+    this.isFocused.set(true);
+  }
+
+  onBlur(): void {
+    this.isFocused.set(false);
+    // @ts-expect-error ControlValueAccessor API
+    this.onTouch();
+  }
+
+  reset(): void {
+    this.searchTerm = '';
+    // @ts-expect-error ControlValueAccessor API
+    this.onChange(this.searchTerm);
+  }
 }
--- angular-hub/src/app/pages/events/index.page.ts
+++ angular-hub/src/app/pages/events/index.page.ts
@@ -1,12 +1,13 @@
-import {Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {EventCardComponent} from "../../components/event-card.component";
-import {injectContentFiles} from "@analogjs/content";
+import {ContentFile, injectContentFiles} from "@analogjs/content";
 import {AsyncPipe, NgForOf} from "@angular/common";
 import {Event} from "../../models/event.model";
 import {Meta, Title} from "@angular/platform-browser";
 import {ActivatedRoute, RouterLink, RouterLinkActive} from "@angular/router";
-import {map} from "rxjs";
+import {combineLatest, map, startWith} from "rxjs";
 import {SearchComponent} from "../../components/search.component";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-evenements',
@@ -18,18 +19,20 @@ import {SearchComponent} from "../../components/search.component";
     AsyncPipe,
     RouterLinkActive,
     SearchComponent,
+    ReactiveFormsModule,
   ],
   template: `
     <h1 class="text-3xl text-start sm:text-5xl font-bold my-8">Agenda</h1>
-    <!-- <app-search></app-search> -->
+    <app-search [formControl]="searchControl"></app-search>
     <nav>
       <ul class="flex gap-4 mb-8">
-        <li><a class="py-2 px-4" routerLink="." routerLinkActive="active" [queryParams]="{state: 'upcoming'}">Upcoming</a></li>
+        <li><a class="py-2 px-4" routerLink="." routerLinkActive="active"
+               [queryParams]="{state: 'upcoming'}">Upcoming</a></li>
         <li><a class="py-2 px-4" routerLink="." routerLinkActive="active" [queryParams]="{state: 'past'}">Past</a></li>
       </ul>
     </nav>
     <ul class="flex flex-col gap-2">
-      <li *ngFor="let event of events$ | async">
+      <li *ngFor="let event of events$ | async; trackBy: trackbyFn">
         <app-event-card
           class="border-2 border-transparent rounded-xl hover:border-gray-200"
           [event]="event"
@@ -47,6 +50,7 @@ import {SearchComponent} from "../../components/search.component";
   `
 })
 export default class EvenementsComponent {
+  searchControl = new FormControl<string>('', {nonNullable: true});
 
   evenements = injectContentFiles<Event>(({filename}) => filename.startsWith('/src/content/events/')).sort(
     (a, b) => new Date(a.attributes.date).getTime() - new Date(b.attributes.date).getTime()
@@ -54,8 +58,18 @@ export default class EvenementsComponent {
   pastEvents = this.evenements.filter(event => new Date(event.attributes.date).getTime() < Date.now());
   upcomingEvents = this.evenements.filter(event => new Date(event.attributes.date).getTime() > Date.now());
 
-  events$ = this.route.queryParamMap.pipe(
-    map(params => params.get('state') === 'past' ? this.pastEvents : this.upcomingEvents),
+  events$ = combineLatest([
+    this.searchControl.valueChanges.pipe(startWith('')),
+    this.route.queryParamMap
+  ]).pipe(
+    map(([searchTerm, params]) => {
+      const state = params.get('state');
+      if (state === 'past') {
+        return this.pastEvents.filter(event => this.filterPredicate(event.attributes, searchTerm));
+      } else {
+        return this.upcomingEvents.filter(event => this.filterPredicate(event.attributes, searchTerm));
+      }
+    })
   );
 
   constructor(
@@ -65,5 +79,20 @@ export default class EvenementsComponent {
   ) {
     title.setTitle('ANGULAR HUB - Curated list of Angular events');
     meta.updateTag({name: 'description', content: 'Curated list of Angular events'});
+  }
+
+  filterPredicate(event: Event, searchTerm: string): boolean {
+    if (searchTerm === '') {
+      return true;
+    }
+
+    const isTitleMatching = event.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const isLocationMatching = event.location.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return isTitleMatching || isLocationMatching;
+  }
+
+  trackbyFn(index: number, event: ContentFile<Event>): string {
+    return event.attributes.title;
   }
 }
