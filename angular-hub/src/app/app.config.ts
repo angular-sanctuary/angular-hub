275f0a74b0f664f70c7b59ee5b437113edc9eb2a
Rafael Mestre <rafaelmestre@outlook.com>
2023-11-21 17:32:00 -0400

    fix: use neutral timezone to prevent date shifts

--- angular-hub/src/app/app.config.ts
+++ angular-hub/src/app/app.config.ts
@@ -1,4 +1,5 @@
 import {ApplicationConfig} from '@angular/core';
+import {DATE_PIPE_DEFAULT_OPTIONS} from '@angular/common';
 import {provideHttpClient, withFetch} from '@angular/common/http';
 import {provideClientHydration} from '@angular/platform-browser';
 import {provideFileRouter} from '@analogjs/router';
@@ -7,6 +8,7 @@ import {provideAnimations} from "@angular/platform-browser/animations";
 
 export const appConfig: ApplicationConfig = {
     providers: [
+        { provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: { timezone: '+0000' } },
         provideFileRouter(),
         provideClientHydration(),
         provideHttpClient(withFetch()),
