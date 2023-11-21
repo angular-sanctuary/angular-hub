ad9a6616657c7c57ad4586228fa9990611a0ecc0
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-11-21 08:45:01 +0100

    feat(cfp): add search feature

--- angular-hub/src/app/pages/cfp/index.page.ts
+++ angular-hub/src/app/pages/cfp/index.page.ts
@@ -1,14 +1,15 @@
 import {Component } from '@angular/core';
 import {EventCardComponent} from "../../components/event-card.component";
-import {injectContentFiles} from "@analogjs/content";
-import {AsyncPipe, NgForOf, NgOptimizedImage} from "@angular/common";
+import {ContentFile, injectContentFiles} from "@analogjs/content";
+import {AsyncPipe, NgForOf} from "@angular/common";
 import {Meta, Title} from "@angular/platform-browser";
 import {ActivatedRoute, RouterLink, RouterLinkActive} from "@angular/router";
 import {CallForPapers} from "../../models/call-for-papers.model";
-import {checkForPrivateExports} from "@angular/compiler-cli/src/ngtsc/entry_point";
 import {CfpCardComponent} from "../../components/call-for-paper-card.component";
 import {SearchComponent} from "../../components/search.component";
-import {map} from "rxjs";
+import {combineLatest, map, startWith} from "rxjs";
+import {FormControl, ReactiveFormsModule} from "@angular/forms";
+import {Event} from "../../models/event.model";
 
 @Component({
   selector: 'app-cfps',
@@ -20,7 +21,8 @@ import {map} from "rxjs";
     SearchComponent,
     RouterLinkActive,
     AsyncPipe,
-    EventCardComponent
+    EventCardComponent,
+    ReactiveFormsModule
   ],
   template: `
     <h1 class="text-3xl text-start sm:text-5xl font-bold my-8">Call For Papers</h1>
@@ -31,9 +33,9 @@ import {map} from "rxjs";
         <li><a class="py-2 px-4" routerLink="." routerLinkActive="active" [queryParams]="{state: 'meetups'}">Meetups</a></li>
       </ul>
     </nav>
-    <!-- <app-search></app-search> -->
+    <app-search [formControl]="searchControl"></app-search>
     <ul class="flex flex-col gap-2">
-      <li *ngFor="let cfp of cfps$ | async">
+      <li *ngFor="let cfp of cfps$ | async; trackby: trackbyFn">
         <app-cfp-card
           class="border-2 border-transparent rounded-xl hover:border-gray-200"
           [cfp]="cfp"
@@ -50,16 +52,23 @@ import {map} from "rxjs";
   `
 })
 export default class CallForPapersComponent {
+  searchControl = new FormControl<string>('', {nonNullable: true});
   cfps = injectContentFiles<CallForPapers>(({filename}) => filename.startsWith('/src/content/cfp/'));
-  cfps$ = this.route.queryParams.pipe(
-    map(({state}) => {
+
+
+  cfps$ = combineLatest([
+    this.searchControl.valueChanges.pipe(startWith('')),
+    this.route.queryParams.pipe(map(({state}) => state))
+  ]).pipe(
+    map(([searchTerm, state]) => {
+      let cfps: ContentFile<CallForPapers>[] = this.cfps;
       if (state === 'meetups') {
-        return this.cfps.filter(({attributes}) => attributes.type === 'meetup');
+        cfps = this.cfps.filter(({attributes}) => attributes.type === 'meetup');
       }
       if (state === 'conferences') {
-        return this.cfps.filter(({attributes}) => attributes.type === 'conference');
+        cfps = this.cfps.filter(({attributes}) => attributes.type === 'conference');
       }
-      return this.cfps;
+      return cfps.filter(cfp => this.filterPredicate(cfp.attributes, searchTerm));
     })
   );
 
@@ -70,5 +79,20 @@ export default class CallForPapersComponent {
   ) {
     title.setTitle('ANGULAR HUB - Curated list of Angular Call For Papers');
     meta.updateTag({name: 'description', content: 'Curated list of Angular Call For Papers'});
+  }
+
+  filterPredicate(event: Event, searchTerm: string): boolean {
+    if (searchTerm === '') {
+      return true;
+    }
+
+    const isTitleMatching = event.title.toLowerCase().includes(searchTerm.toLowerCase());
+    const isLocationMatching = event.location.toLowerCase().includes(searchTerm.toLowerCase());
+
+    return isTitleMatching || isLocationMatching;
+  }
+
+  trackbyFn(index: number, cfp: CallForPapers): string {
+    return cfp.title;
   }
 }
