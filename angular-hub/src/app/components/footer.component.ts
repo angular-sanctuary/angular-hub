0663df34b7378b001598da29710bbdcae079267b
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-11-22 15:19:08 +0100

    refactor: move footer in navigation menu on large screens

--- angular-hub/src/app/components/footer.component.ts
+++ angular-hub/src/app/components/footer.component.ts
@@ -1,11 +1,15 @@
-import {ChangeDetectionStrategy, Component} from "@angular/core";
+import {ChangeDetectionStrategy, Component, Input} from "@angular/core";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <footer class="flex justify-center items-center gap-4 mt-16">
+    <footer
+        class="flex justify-center items-center gap-4 mt-16"
+        [ngClass]="{'flex-col-reverse pb-4': !isHandset}"
+    >
       <a class="underline bold" href="https://analogjs.org/" target="_blank">Powered by Analog</a>
       <a class="contribute-btn" href="https://github.com/angular-sanctuary/angular-hub" target="_blank">Contribute</a>
     </footer>
@@ -21,6 +25,10 @@ import {ChangeDetectionStrategy, Component} from "@angular/core";
       }
     `,
   ],
+  imports: [
+    NgClass
+  ]
 })
 export class FooterComponent {
+  @Input() isHandset = true;
 }
