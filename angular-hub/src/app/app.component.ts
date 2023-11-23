ab789700a5085bf7dc12646015f39dc7397186b2
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-11-23 13:32:16 +0100

    chore: add pre-commit hooks

--- angular-hub/src/app/app.component.ts
+++ angular-hub/src/app/app.component.ts
@@ -1,38 +1,39 @@
-import {Component} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
-import {NavigationComponent} from "./components/navigation/navigation.component";
-import {DomSanitizer} from "@angular/platform-browser";
-import {MatIconRegistry} from "@angular/material/icon";
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
-    selector: 'angular-hub-root',
-    standalone: true,
-    imports: [RouterOutlet, NavigationComponent],
-    template: `
-        <app-navigation class="h-full">
-        </app-navigation>
+  selector: 'angular-hub-root',
+  standalone: true,
+  imports: [RouterOutlet, NavigationComponent],
+  template: ` <app-navigation class="h-full"> </app-navigation> `,
+  styles: [
+    `
+      :host {
+        width: 100%;
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+      }
     `,
-    styles: [
-        `
-            :host {
-                width: 100%;
-                text-align: center;
-                display: flex;
-                flex-direction: column;
-            }
-        `,
-    ],
+  ],
 })
 export class AppComponent {
-    icons = ['menu', 'settings'];
+  icons = ['menu', 'settings'];
 
-    constructor(private readonly iconRegistry: MatIconRegistry,
-                private readonly sanitizer: DomSanitizer) {
-        this.icons.forEach(icon => {
-            this.iconRegistry.addSvgIcon(
-                icon,
-                this.sanitizer.bypassSecurityTrustResourceUrl(`assets/icons/${icon}.svg`)
-            );
-        });
-    }
+  constructor(
+    private readonly iconRegistry: MatIconRegistry,
+    private readonly sanitizer: DomSanitizer
+  ) {
+    this.icons.forEach((icon) => {
+      this.iconRegistry.addSvgIcon(
+        icon,
+        this.sanitizer.bypassSecurityTrustResourceUrl(
+          `assets/icons/${icon}.svg`
+        )
+      );
+    });
+  }
 }
