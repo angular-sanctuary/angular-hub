09e526d3cc7b3016b1049a5e0556ce6356773dc5
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2023-11-25 22:04:09 +0100

    feat: add pwa support

--- angular-hub/src/app/app.component.ts
+++ angular-hub/src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './components/navigation/navigation.component';
 import { DomSanitizer } from '@angular/platform-browser';
 import { MatIconRegistry } from '@angular/material/icon';
+import { SwUpdate, VersionReadyEvent } from '@angular/service-worker';
+import { filter, switchMap } from 'rxjs';
 
 @Component({
   selector: 'angular-hub-root',
@@ -25,7 +27,9 @@ export class AppComponent {
 
   constructor(
     private readonly iconRegistry: MatIconRegistry,
-    private readonly sanitizer: DomSanitizer
+    private readonly sanitizer: DomSanitizer,
+    private readonly swUpdate: SwUpdate,
+    private readonly router: Router
   ) {
     this.icons.forEach((icon) => {
       this.iconRegistry.addSvgIcon(
@@ -35,5 +39,15 @@ export class AppComponent {
         )
       );
     });
+
+    router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        switchMap(() => this.swUpdate.versionUpdates),
+        filter((evt): evt is VersionReadyEvent => evt.type === 'VERSION_READY')
+      )
+      .subscribe(() => {
+        document.location.reload();
+      });
   }
 }
--- angular-hub/src/app/app.config.ts
+++ angular-hub/src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { provideClientHydration } from '@angular/platform-browser';
@@ -6,6 +6,7 @@ import { provideFileRouter } from '@analogjs/router';
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { withViewTransitions } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -15,5 +16,9 @@ export const appConfig: ApplicationConfig = {
     provideHttpClient(withFetch()),
     provideContent(withMarkdownRenderer()),
     provideAnimationsAsync(),
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
