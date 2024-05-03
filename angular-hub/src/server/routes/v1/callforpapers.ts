4b9f373122b27151f8f999c32b3654740a9efc37
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-03 18:47:00 +0200

    feat: update callforpapers endpoint

--- angular-hub/src/server/routes/v1/callforpapers.ts
+++ angular-hub/src/server/routes/v1/callforpapers.ts
@@ -1,6 +1,27 @@
 import { defineEventHandler } from 'h3';
 import communities from '../../../public/assets/data/community.json';
 
-export default defineEventHandler(() => {
-  return communities.filter((community) => community.callForPapers);
+export default defineEventHandler((event) => {
+  const searchParams = event.path.split('?')[1];
+  const urlParams = new URLSearchParams(searchParams);
+  const type = urlParams.get('type');
+
+  const filteredCommunities = communities.filter((community) => {
+    if (!type) {
+      return community.callForPapers;
+    }
+
+    if (type === 'meetup') {
+      return community.callForPapers && community.type === 'meetup';
+    }
+
+    if (type === 'conference') {
+      const openCFP = community.events.find(
+        (event) => new Date(event.callForPapersEndDate) > new Date(),
+      );
+      return community.type === 'conference' && openCFP;
+    }
+  });
+
+  return filteredCommunities;
 });
