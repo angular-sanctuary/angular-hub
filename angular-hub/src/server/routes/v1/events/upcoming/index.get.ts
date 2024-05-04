af8a4d0334e45da03a1500f47f0ff83471c70c0a
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-04 17:08:34 +0200

    feat: add events endpoints

---
+++ angular-hub/src/server/routes/v1/events/upcoming/index.get.ts
@@ -0,0 +1,23 @@
+import { defineEventHandler } from 'h3';
+import communities from '../../../../../public/assets/data/community.json';
+
+export default defineEventHandler(() => {
+  // return all events
+  const events = communities
+    .map((community) => {
+      const { _, ...communityMeta } = community;
+      return community.events
+        .map((event) => {
+          return {
+            ...event,
+            community: communityMeta,
+          };
+        })
+        .flat();
+    })
+    .flat()
+    .filter((event) => new Date(event.date).getTime() > Date.now())
+    .sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+
+  return events;
+});
