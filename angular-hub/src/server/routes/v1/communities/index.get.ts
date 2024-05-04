1b21fc8066ac300e361c8ac5d48c711e26f0db2a
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-04 16:43:58 +0200

    feat: add upcoming events

--- angular-hub/src/server/routes/v1/communities/index.get.ts
+++ angular-hub/src/server/routes/v1/communities/index.get.ts
@@ -1,4 +1,4 @@
 import { defineEventHandler } from 'h3';
-import communities from '../../../public/assets/data/community.json';
+import communities from '../../../../public/assets/data/community.json';
 
 export default defineEventHandler(() => communities);
