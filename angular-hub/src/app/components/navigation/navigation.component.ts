ff39fb22334b5b43d5f2aa953eeda408cea03595
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-05 18:50:36 +0200

    feat: add links

--- angular-hub/src/app/components/navigation/navigation.component.ts
+++ angular-hub/src/app/components/navigation/navigation.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-navigation',
@@ -14,6 +15,7 @@ import { SidebarModule } from 'primeng/sidebar';
     ButtonModule,
     SidebarModule,
     RouterLinkActive,
+    DividerModule,
   ],
 })
 export class NavigationComponent {
