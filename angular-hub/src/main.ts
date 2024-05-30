2f111a2dc14b253bc157fae978a4da01442979ce
Chau Tran <nartc7789@gmail.com>
2024-05-30 15:54:31 -0500

    chore: make my lottery factory happy

--- angular-hub/src/main.ts
+++ angular-hub/src/main.ts
@@ -1,6 +1,6 @@
 import 'zone.js';
 import { bootstrapApplication } from '@angular/platform-browser';
-import App from './app/App.analog';
+import App from './app/app.analog';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
