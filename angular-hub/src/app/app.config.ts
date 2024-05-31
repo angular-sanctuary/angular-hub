f61c3b4e7b4840913855f95dfb7e18444dffc968
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2024-05-31 11:52:31 +0200

    feat: add event replay

--- angular-hub/src/app/app.config.ts
+++ angular-hub/src/app/app.config.ts
@@ -1,7 +1,10 @@
 import { ApplicationConfig, isDevMode } from '@angular/core';
 import { DATE_PIPE_DEFAULT_OPTIONS } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideClientHydration } from '@angular/platform-browser';
+import {
+  provideClientHydration,
+  withEventReplay,
+} from '@angular/platform-browser';
 import { provideFileRouter } from '@analogjs/router';
 import { provideContent, withMarkdownRenderer } from '@analogjs/content';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
@@ -15,7 +18,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     { provide: DATE_PIPE_DEFAULT_OPTIONS, useValue: { timezone: '+0000' } },
     provideFileRouter(withViewTransitions(), withComponentInputBinding()),
-    provideClientHydration(),
+    provideClientHydration(withEventReplay()),
     provideHttpClient(withFetch()),
     provideContent(withMarkdownRenderer()),
     provideAnimationsAsync(),
