21bff5c121e22d1615c5207414d9aee5a6d0002e
Gerome Grignon <gerome.grignon.lp2@gmail.com>
2025-07-10 01:09:34 +0200

    feat: add podcasts route with prerender mode to server routes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -6,6 +6,10 @@ export const serverRoutes: ServerRoute[] = [
     renderMode: RenderMode.Server,
   },
   {
+    path: 'podcasts',
+    renderMode: RenderMode.Prerender,
+  },
+  {
     path: '**',
     renderMode: RenderMode.Server,
   },
